74b94a2be28b0b61d2413c1fdb9131464b0c75db
kazuyaTakahashi1988 <kazuya.takahashi.631130@gmail.com>
2025-08-04 00:25:14 +0900

    調整

--- app/components/layout/index.tsx
+++
@@ -1,2 +0,0 @@
-export * from './primaryLayout';
-export * from './secondaryLayout';
--- app/components/layout/secondaryLayout/index.tsx
+++ app/components/layout/secondaryLayout/index.tsx
@@ -1,1 +1,20 @@
-export { default as SecondaryLayout } from './_layout';
+import { StyleSheet, View, Text, Button } from 'react-native';
+
+const SecondaryLayout = () => {
+  return (
+    <View style={styles.container}>
+      <Text>SecondaryLayout</Text>
+    </View>
+  );
+}
+
+export default SecondaryLayout;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
