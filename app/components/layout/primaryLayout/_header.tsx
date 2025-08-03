b18ce5cb9b92c472369ccd0ae136ace2422dc443
kazuyaTakahashi1988 <kazuya.takahashi.631130@gmail.com>
2025-08-04 01:04:03 +0900

    調整

--- app/components/layout/primaryLayout/_header.tsx
+++ app/components/layout/primaryLayout/_header.tsx
@@ -1,13 +1,13 @@
 import { StyleSheet, View, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { Logo, IconCaution } from '../../svg'
+import { Logo, IconInfo } from '../../svg'
 
 const Header: React.FC = () => {
 const navigation = useNavigation<any>();
   return (
     <View style={styles.header}>
       <Text style={styles.headerItem}><Logo /></Text>
-      <Text style={styles.headerItem} onPress={() => navigation.navigate('about')}><IconCaution /></Text>
+      <Text style={styles.headerItem} onPress={() => navigation.navigate('about')}><IconInfo /></Text>
     </View>
   );
 }
