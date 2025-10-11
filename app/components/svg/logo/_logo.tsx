309d61e8cf352248882deea95201bd17a40c828a
kazuyaTakahashi1988 <kazuya.takahashi.631130@gmail.com>
2025-10-11 16:22:55 +0900

    eslint.useFlatConfig削除

--- app/components/svg/logo/_logo.tsx
+++ app/components/svg/logo/_logo.tsx
@@ -5,7 +5,7 @@ import type React from 'react';
 
 const Logo: React.FC<TypeSvg> = ({ color = '#000', size = 100 }) => (
   <Svg width={size} height={size} viewBox='0 0 200 60' fill='none'>
-    <Text fill={color} fontSize='48' fontWeight='bold' x='0' y='45'>
+    <Text fill={color} fontSize='48' fontWeight='bold' transform='translate(0 45)'>
       LOGO
     </Text>
   </Svg>
