895319394af7c834984b225aaf08326e98cf514f
kazuyaTakahashi1988 <kazuya.takahashi.631130@gmail.com>
2025-10-15 00:12:54 +0900

    不要タイプ削除

--- app/lib/types/typeNavigation/_navigation.ts
+++ app/lib/types/typeNavigation/_navigation.ts
@@ -1,5 +1,1 @@
-import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
-
 export type TypeRootList = Record<string, undefined | { [key: string]: string | object }>;
-
-export type TypeNavigation = NativeStackNavigationProp<TypeRootList>;
