946a8d275422d2f34e3c7a5d8170dc8459d8f4f7
kazuyaTakahashi1988 <57883262+kazuyaTakahashi1988@users.noreply.github.com>
2025-11-16 08:27:03 +0900

    Configure Storybook Babel for reanimated

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,23 @@
+import type { TransformOptions } from '@babel/core';
 import type { StorybookConfig } from '@storybook/react-native-web-vite';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@chromatic-com/storybook',
+    '@storybook/addon-docs',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-a11y',
+    '@storybook/addon-vitest',
   ],
-  "addons": [
-    "@chromatic-com/storybook",
-    "@storybook/addon-docs",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
-  ],
-  "framework": {
-    "name": "@storybook/react-native-web-vite",
-    "options": {}
-  }
+  framework: {
+    name: '@storybook/react-native-web-vite',
+    options: {},
+  },
+  babel: (options: TransformOptions = {}) => ({
+    ...options,
+    plugins: [...(options.plugins ?? []), 'react-native-reanimated/plugin'],
+  }),
 };
+
 export default config;
