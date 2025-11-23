5f71bb88174b0d5f425774932452eac4c0a1c31d
kazuyaTakahashi1988 <kazuya.takahashi.631130@gmail.com>
2025-11-23 19:37:46 +0900

    Revert "調整"
    This reverts commit ab2f8871bde9059c550aa37d21a0ae2b0d34ddf2.

--- app/services/apiHelper/_execute.ts
+++ app/services/apiHelper/_execute.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 import type { TypeOptions } from '../../lib/types/typeService';
-import type { AxiosRequestConfig, AxiosResponse } from 'axios';
+import type { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 
 const DEFAULT_API_BASE_URL = 'https://wp.empty-service.com';
 
@@ -57,9 +57,12 @@ export const execute = async <TResponse = unknown, TRequest = unknown>(
 
   try {
     return await axios.request<TResponse>(requestConfig);
-  } catch (err) {
-    console.error('API request failed', err);
-    throw err;
+  } catch (error) {
+    const axiosError = error as AxiosError;
+    const message = axiosError.response?.data ?? axiosError.message;
+
+    console.error('API request failed', message);
+    throw axiosError;
   }
 };
 
