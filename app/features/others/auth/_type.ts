ccd0772f838301906342db05ab9bc743773c64fa
kazuyaTakahashi1988 <kazuya.takahashi.631130@gmail.com>
2025-11-28 20:30:57 +0900

    調整

--- app/features/others/auth/_type.ts
+++ app/features/others/auth/_type.ts
@@ -1,6 +1,10 @@
 import type { FieldValues, UseFormReturn } from 'react-hook-form';
 
 export type TypeTabKey = 'signIn' | 'signUp' | 'verify';
+export type TypeResult = {
+  type?: 'success' | 'error';
+  message?: string;
+};
 
 export type TypeAuthForm<T extends FieldValues> = {
   form: UseFormReturn<T>;
