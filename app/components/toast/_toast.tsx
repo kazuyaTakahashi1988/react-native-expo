592324bf62341a6c0da4e919bde297fe37aa6456
kazuyaTakahashi1988 <57883262+kazuyaTakahashi1988@users.noreply.github.com>
2025-12-06 15:01:44 +0900

    Replace toast reanimated animation with Animated

--- app/components/toast/_toast.tsx
+++ app/components/toast/_toast.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import { Modal, Platform, StyleSheet, Text, View } from 'react-native';
-import Animated, {
-  interpolate,
-  useAnimatedStyle,
-  useSharedValue,
-  withTiming,
-} from 'react-native-reanimated';
+import { Animated, Modal, Platform, StyleSheet, Text, View } from 'react-native';
 
 import { color } from '../../lib/mixin';
 
@@ -99,7 +93,7 @@ const useToastController = ({
   onShow,
   position = 'bottom',
 }: UseToastControllerProps) => {
-  const opacity = useSharedValue(0);
+  const opacity = React.useRef(new Animated.Value(0)).current;
   const [mounted, setMounted] = React.useState(visible);
   const [modalVisible, setModalVisible] = React.useState(visible);
 
@@ -121,13 +115,21 @@ const useToastController = ({
       setModalVisible(true);
       setMounted(true);
       onShowRef.current?.();
-      opacity.value = withTiming(1, { duration: animationDuration });
+      Animated.timing(opacity, {
+        toValue: 1,
+        duration: animationDuration,
+        useNativeDriver: true,
+      }).start();
 
       timer = setTimeout(() => {
         onHideRef.current?.();
       }, duration);
     } else if (mounted) {
-      opacity.value = withTiming(0, { duration: animationDuration });
+      Animated.timing(opacity, {
+        toValue: 0,
+        duration: animationDuration,
+        useNativeDriver: true,
+      }).start();
 
       timer = setTimeout(() => {
         setMounted(false);
@@ -142,18 +144,18 @@ const useToastController = ({
     };
   }, [duration, mounted, opacity, visible]);
 
-  const animatedStyle = useAnimatedStyle(() => {
-    const startOffset = getStartOffset(position);
-
-    return {
-      opacity: opacity.value,
-      transform: [
-        {
-          translateY: interpolate(opacity.value, [0, 1], [startOffset, 0]),
-        },
-      ],
-    };
-  });
+  const startOffset = getStartOffset(position);
+  const animatedStyle = {
+    opacity,
+    transform: [
+      {
+        translateY: opacity.interpolate({
+          inputRange: [0, 1],
+          outputRange: [startOffset, 0],
+        }),
+      },
+    ],
+  };
 
   return { mounted, animatedStyle, modalVisible };
 };
